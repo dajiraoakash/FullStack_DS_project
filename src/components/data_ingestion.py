04a97a0c1d7d75d68d528d3b155f462eeffa6a7c
dajiraoakash <dajiraoakash@gmail.com>
2025-03-03 20:13:50 -0500

    data transformarion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+
 @dataclass 
 #If we use this decorater @dataclass we can directly define class variable without __init__ constructor. This decorater can be used only when you have a class that has only variables to be defined inside.
 
@@ -44,6 +46,9 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
 
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -11,6 +11,8 @@ from src.exception import CustomException
 from src.logger import logging
 import os
 
+from src.utils import save_object
+
 
 @dataclass
 class DataTransformationConfig:
@@ -44,9 +46,9 @@ class DataTransformation:
             )
             categorical_pipeline=Pipeline(
                 steps=[
-                    ("imputer",SimpleImputer(strategy="most_frequent"))
-                    ("one_hot_encoder", OneHotEncoder())
-                    ("scaler", StandardScaler())
+                    ("imputer",SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder", OneHotEncoder()),
+                    ("scaler", StandardScaler(with_mean=False))
                 ]
 
             )
@@ -75,17 +77,42 @@ class DataTransformation:
 
             logging.info("Obtaining Preprocessor Object")
 
-            preprocessing_obj = get_data_transformer_object()
+            preprocessing_obj = self.get_data_transformer_object()
 
             target_column_name="math_score"
             numerical_columns=["writing_score", "reading_score"]
 
             input_feature_train_df = train_df.drop(columns=[target_column_name],axis=1)
-            
-            
+            target_feature_train_df=train_df[target_column_name]
 
+            input_feature_test_df = test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
 
+            logging.info(f"Applying preprocessing object on training and testing dataframes")
 
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr=np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+
+            test_arr=np.c_[
+                input_feature_test_arr, np.array(target_feature_test_df)
+            ]
+
+            logging.info(f"Saved preprocesssing object.")
+
+            save_object(
+                file_path = self.data_tranformation_config.preprcessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_tranformation_config.preprcessor_obj_file_path,
+            )
 
 
         except Exception as e:
