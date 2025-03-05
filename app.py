22f097009240df1301850469e0db2bfdc266b969
dajiraoakash <dajiraoakash@gmail.com>
2025-03-05 18:57:47 -0500

    Flask app

---
+++ app.py
@@ -0,0 +1,24 @@
+import pickle
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+
+
+
+application=Flask(__name__)
+
+app=application
+
+## Route for a home page
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/predictdata",methods=["GET","POST"])
+def predict_datapoint():
+    if request.method=="GET":
+        reurn render_template("home.html")
+    else:
+        pass
